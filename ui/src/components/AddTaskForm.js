52027af37bc2e95b76d01ed98696dd60d42d2080
Rochak <rochakbhattarai11@gmail.com>
2024-03-11 11:19:06 +0545

    sssssssss

--- ui/src/components/AddTaskForm.js
+++ ui/src/components/AddTaskForm.js
@@ -4,7 +4,7 @@ import { Button, Typography } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import axios from "axios";
 import { API_URL } from "../utils";
-import ReactMarkdown from "react-markdown";
+// import ReactMarkdown from "react-markdown";
 
 export const AddTaskForm = ({ fetchTasks }) => {
   const [newTask, setNewTask] = useState("");
